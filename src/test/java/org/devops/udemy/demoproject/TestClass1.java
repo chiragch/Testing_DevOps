c11885a12197e21f94fa6b84472e1c8906864139
cchopda62@gmail.com <cchopda62@gmail.com>
2019-02-19 13:34:44 -0500

    Added Test case

--- src/test/java/org/devops/udemy/demoproject/TestClass1.java
+++ src/test/java/org/devops/udemy/demoproject/TestClass1.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -22,11 +23,17 @@ public class TestClass1 {
 	public void Test1() {
 		driver.navigate().to("http://localhost:4200/");
 		System.out.println("Test 1 title is" + driver.getTitle());
+		
+		
+		
 	}
 	@Test
 	public void Test2() {
 		driver.navigate().to("http://localhost:4200/");
 		System.out.println("Test 2 title is" + driver.getTitle());
+		String actualTitle = "NoDQ - Wrestling PodCast";
+		Assert.assertEquals(driver.getTitle(), actualTitle);
+		
 	}
 	@Test
 	public void Test3() {
