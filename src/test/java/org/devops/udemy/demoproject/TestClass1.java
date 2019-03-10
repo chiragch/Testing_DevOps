24b4e04896846ae3938937891411946147a572e8
cchopda62@gmail.com <cchopda62@gmail.com>
2019-03-10 16:05:15 -0400

    Selenium grid

--- src/test/java/org/devops/udemy/demoproject/TestClass1.java
+++ src/test/java/org/devops/udemy/demoproject/TestClass1.java
@@ -1,9 +1,14 @@
 package org.devops.udemy.demoproject;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
@@ -13,9 +18,13 @@ public class TestClass1 {
 	
 	public static WebDriver driver;
 	@BeforeMethod
-	public void launchDriver() {
+	public void launchDriver() throws MalformedURLException {
+		DesiredCapabilities dr = null;
+		dr = DesiredCapabilities.chrome();
+		dr.setBrowserName("chrome");
+		dr.setPlatform(Platform.WIN10);
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\cchop\\OneDrive\\Desktop\\Tools\\chrome versions\\chromedriver_win32 (2)\\chromedriver.exe");
-		driver = new ChromeDriver();
+		driver = new RemoteWebDriver(new URL("http://172.20.5.87:4444/wd/hub"), dr);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().fullscreen();
 	}
